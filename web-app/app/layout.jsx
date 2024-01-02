52e325421cf26315b04b2a6d0d8b396387100a13
Jonathan E <jonathan@jonathande.dev>
2024-01-02 20:13:22 +0000

    Revert "Merge branch 'static-home' into 'main'"
    This reverts merge request !2

--- web-app/app/layout.jsx
+++ web-app/app/layout.jsx
@@ -1,13 +1,7 @@
 import React from "react"
 
-// Global styles
 import "@/src/fonts/fonts.css";
 import "@/src/globals.css";
-
-// Font Awesome
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
 
 // Page metadata
 export const metadata = {
@@ -26,19 +20,10 @@ export const metadata = {
   }
 };
 
-import Header from "@/src/components/ui/Header/Header.jsx";
-import MessageButton from "@/src/components/ui/MessageButton.jsx";
-import Footer from "@/src/components/ui/Footer/Footer.jsx";
-
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
-        <Header/>
-        {/* <MessageButton/> */}
-        {children}
-        <Footer/>
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
